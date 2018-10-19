15591f636e82a1671b593c283df3a745e9a00a66
dejavn <dejavn@gmail.com>
2018-10-19 14:45:36 +0800

    update media path

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'blog/static/media')
 
 MAX_UPLOAD_SIZE = 150100
 
