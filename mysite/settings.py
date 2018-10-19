1b290926bb3c5177777728df386b5a580cf516a7
dejavn <dejavn@gmail.com>
2018-10-20 00:25:10 +0800

    update setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yo4qzu*2k%go$$&&6p36l!rwwv4gl1$vr$xs94=gx!6wqpqv=5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
